4c9f10db23a9c169556588115ebd83fc91d422b2
p1aaku <211001042@rajalakshmi.edu.in>
2024-07-06 00:37:31 +0530

    updated test

--- Linked_List/testList.java
+++ Linked_List/testList.java
@@ -1,5 +1,7 @@
 package Linked_List;
+
 import java.util.*;
+
 public class testList {
     public static void main(String[] args) {
         List<List<Integer>> x = new ArrayList<>();
@@ -12,6 +14,7 @@ public class testList {
         y.add(4);
         y.add(5);
         y.add(6);
+        y.add(7);
         x.add(y);
         System.out.println(x);
     }
